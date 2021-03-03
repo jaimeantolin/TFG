a9ec8a3de0cdd9e45640cdc7c5c685af2d74fddd
jaimeantolin <jaiman01@ucm.es>
2021-03-03 23:28:28 +0100

    Login, logout y registro basico funcionando
    Part 4/6

--- app/src/main/java/com/smallacademy/userroles/Login.java
+++ app/src/main/java/com/smallacademy/userroles/Login.java
@@ -1,30 +1,70 @@
 package com.smallacademy.userroles;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Login extends AppCompatActivity {
     EditText email,password;
     Button loginBtn,gotoRegister;
     boolean valid = true;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
-        email = findViewById(R.id.loginEmail); // cambiado pero no estoy seguro
+
+        email = findViewById(R.id.loginEmail);
         password = findViewById(R.id.loginPassword);
         loginBtn = findViewById(R.id.loginBtn);
         gotoRegister = findViewById(R.id.gotoRegister);
 
-        checkField(email);
-        checkField(password);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkField(email);
+                checkField(password);
+
+
+                if(valid){
+                    fAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(Login.this, "Logged in Succesfully", Toast.LENGTH_SHORT).show();
+                            checkUserAccessLevel(authResult.getUser().getUid());
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT ).show(); // displays error message
+                        }
+                    });
+
+
+                }
+            }
+        });
 
 
         gotoRegister.setOnClickListener(new View.OnClickListener() {
@@ -40,6 +80,32 @@ public class Login extends AppCompatActivity {
 
     }
 
+    private void checkUserAccessLevel(String uid) {
+        DocumentReference df = fStore.collection("Users").document(uid);
+        // extract the data from the document
+
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d("TAG", "onSuccess: " + documentSnapshot.getData());
+
+                //identify the user access level
+
+                if(documentSnapshot.getString("isAdmin") != null){
+                    //user is admin
+                    startActivity(new Intent(getApplicationContext(),AdminActivity.class));
+                    finish();
+                }
+
+                if(documentSnapshot.getString("isUser") != null){
+                    //user is not admin
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                }
+            }
+        });
+    }
+
     public boolean checkField(EditText textField){
         if(textField.getText().toString().isEmpty()){
             textField.setError("Error");
@@ -49,5 +115,14 @@ public class Login extends AppCompatActivity {
         }
 
         return valid;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
     }
 }
